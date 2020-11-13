4baebca667fda7fddf83985c0afe0d7fbae2cee7
Anurag Pamuru <anuragpamuru0129@gmail.com>
2020-11-12 17:20:37 -0800

    Update data.py

--- src/data/data.py
+++ src/data/data.py
@@ -3,54 +3,56 @@ import pandas as pd
 import torch
 
 def encode_onehot(labels):
-    classes = set(labels)
-    classes_dict = {c: np.identity(len(classes))[i, :] for i, c in
-                    enumerate(classes)}
+    #ordinal
+    nrows = len(labels)
+    unique = set(labels)
+    classes_dict = {c: np.identity(len(unique))[i, :] for i, c in
+                    enumerate(unique)}
     labels_onehot = np.array(list(map(classes_dict.get, labels)),
                              dtype=np.int32)
     return labels_onehot
 
-def get_data(d1_address, d2_address, keys_address):
+def encode(features, encoding_config):
+    encoded_data = []
+    for encoding_instruction in encoding_config:
+        encoder_col = features[encoding_instruction["column"]]
+        if encoding_instruction["encoding_types"] == "one_hot":
+            encoder_col = encode_onehot(encoder_col)
+        encoded_data.append(encoder_col)
+    return encoded_data if len(encoded_data) > 1 else encoded_data[0]
+
+def get_adj(edges, directed):    
+    rows = edges[0]
+    cols = edges[1]
     
-    d1 = (pd.read_csv(d1_address, sep ='\t', header=None))
-    d2 = pd.read_csv(d2_address, sep ='\t', header=None)
-    keys = pd.read_csv(keys_address, sep ='\t', header=None)[[0,1434]]
-
-    index = d1[0]
-
+    index = list(set(edges[0]).union(set(edges[1])))
+    n_papers = len(index)
+    
     id2index = {}
     for i in np.arange(len(index)):
         id2index[index[i]] = i
         i += 1
-
-    n_papers = len(id2index)
-
-    labels = d1[1434]
-    label2index = {
-            'Case_Based': 0,
-            'Genetic_Algorithms': 1,
-            'Neural_Networks': 2,
-            'Probabilistic_Methods': 3,
-            'Reinforcement_Learning': 4,
-            'Rule_Learning': 5,
-            'Theory': 6
-    }
-
-    label_ind = np.zeros(n_papers)
-    for i in np.arange(n_papers):
-        label_ind[i] = label2index[labels[i]]
-
+    
     adj = np.zeros((n_papers, n_papers), dtype='float32')
 
-    rows = d2[0]
-    cols = d2[1]
+    for i in range(len(edges)):
+        if directed:
+            adj[id2index[rows[i]], id2index[cols[i]]]  = 1.0
+        else:
+            adj[id2index[rows[i]], id2index[cols[i]]]  = 1.0 
+            adj[id2index[cols[i]], id2index[rows[i]]]  = 1.0 
+    return adj
 
-    for i in np.arange(len(d2)):
-        adj[id2index[rows[i]], id2index[cols[i]]]  = 1.0 # directed graph
+def get_data(feature_address, edges_address, encoding_config, directed):
+    features = pd.read_csv(feature_address, sep ='\t', header=None)
+    edges = pd.read_csv(edges_address, sep ='\t', header=None)
 
-    features = np.array(d1.iloc[:, 1:1434])
-
-    encoded_labels = np.array(encode_onehot(label_ind))
+    index = features[0]
+    n_papers = len(features)
+    
+    adj = get_adj(edges, directed)
+    
+    encoded_labels = encode(features, encoding_config)
     #add identity matrix to adjacency matrix
     adj_added = adj + np.eye(adj.shape[0])
 
@@ -65,6 +67,7 @@ def get_data(d1_address, d2_address, keys_address):
     adj_hat[adj_hat > 0.0001] = adj_hat[adj_hat > 0.0001] - 0.2
 
     #put numpy arrays to tensors
+    features = np.array(features.iloc[:, 1:1434])
     features = torch.FloatTensor(features)
     labels = torch.LongTensor(np.where(encoded_labels)[1])
 
